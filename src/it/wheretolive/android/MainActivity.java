036271cb766625d90242e4195be761a7cc66bc2c
Andrea Scarpino <me@andreascarpino.it>
2014-09-10 20:21:30 +0200

    Enable JavaScript

--- src/it/wheretolive/android/MainActivity.java
+++ src/it/wheretolive/android/MainActivity.java
@@ -8,19 +8,23 @@
 
 package it.wheretolive.android;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 
 public class MainActivity extends Activity {
 
-	@Override
+	@SuppressLint("SetJavaScriptEnabled") @Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		
 		setContentView(R.layout.main);
 
 		WebView myWebView = (WebView) findViewById(R.id.webview);
+		WebSettings webSettings = myWebView.getSettings();
+		webSettings.setJavaScriptEnabled(true);
 		myWebView.loadUrl("http://www.wheretolive.it/");
 	}
 }

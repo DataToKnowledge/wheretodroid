91a9848e504d26befbee8f1838c37fb5eb063f20
Andrea Scarpino <me@andreascarpino.it>
2014-09-10 20:25:02 +0200

    Use back key to go back in history

--- src/it/wheretolive/android/MainActivity.java
+++ src/it/wheretolive/android/MainActivity.java
@@ -11,26 +11,43 @@ package it.wheretolive.android;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class MainActivity extends Activity {
 
-	@SuppressLint("SetJavaScriptEnabled") @Override
+	private WebView myWebView;
+
+	@SuppressLint("SetJavaScriptEnabled")
+	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+
 		setContentView(R.layout.main);
 
-		WebView myWebView = (WebView) findViewById(R.id.webview);
-		
+		myWebView = (WebView) findViewById(R.id.webview);
+
 		WebSettings webSettings = myWebView.getSettings();
 		webSettings.setJavaScriptEnabled(true);
-		
+
 		// Open links into this app
 		myWebView.setWebViewClient(new WebViewClient());
-		
+
 		myWebView.loadUrl("http://www.wheretolive.it/");
+	}
+
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		// Check if the key event was the Back button and if there's history
+		if ((keyCode == KeyEvent.KEYCODE_BACK) && myWebView.canGoBack()) {
+			myWebView.goBack();
+			return true;
+		}
+
+		// If it wasn't the Back key or there's no web page history, bubble up
+		// to the default system behavior (probably exit the activity)
+		return super.onKeyDown(keyCode, event);
 	}
 }

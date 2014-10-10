450358757c391ebab29f0b1b233bf4a96791b10d
Andrea Scarpino <andrea@datatoknowledge.it>
2014-10-10 16:58:01 +0200

    Open external links into system browser
    Fixes #2

--- src/it/wheretolive/android/MainActivity.java
+++ src/it/wheretolive/android/MainActivity.java
@@ -3,19 +3,25 @@
  * @copyright DataToKnowledge S.r.l.s.- September 2014 - All Rights Reserved.
  * Unauthorized copying of this file, via any medium is strictly prohibited.
  * @license Proprietary and confidential
- * @author Andrea Scarpino <andrea@datatoknowledge.it>
  */
 
 package it.wheretolive.android;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
+/**
+ *
+ * @author Andrea Scarpino <andrea@datatoknowledge.it>
+ *
+ */
 public class MainActivity extends Activity {
 
 	private WebView myWebView;
@@ -33,7 +39,7 @@ public class MainActivity extends Activity {
 		webSettings.setJavaScriptEnabled(true);
 
 		// Open links into this app
-		myWebView.setWebViewClient(new WebViewClient());
+		myWebView.setWebViewClient(new MyWebViewClient());
 
 		myWebView.loadUrl("http://www.wheretolive.it/");
 	}
@@ -49,5 +55,23 @@ public class MainActivity extends Activity {
 		// If it wasn't the Back key or there's no web page history, bubble up
 		// to the default system behavior (probably exit the activity)
 		return super.onKeyDown(keyCode, event);
+	}
+
+	private class MyWebViewClient extends WebViewClient {
+
+		@Override
+		public boolean shouldOverrideUrlLoading(WebView view, String url) {
+			if (Uri.parse(url).getHost().equals("www.wheretolive.it")) {
+				// This is my web site, so do not override; let my WebView load
+				// the page
+				return false;
+			}
+
+			// Otherwise, the link is not for a page on my site, so launch
+			// another Activity that handles URLs
+			Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+			startActivity(intent);
+			return true;
+		}
 	}
 }
